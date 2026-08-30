c8aba64c6d8561ba406d52f9b1fa6a5a27367a0d
kilfaaa <tanya06040086@gmail.com>
2026-08-30 21:41:19 +0300

    add task saving
    Добавлена функция сохранения списка задач.

--- main.py
+++ main.py
@@ -32,6 +32,11 @@ def show_tasks(tasks):
 
             print(f"\n{status} ID: {task_id} \nНазвание: {task_title} \nОписание: {task_description}\n")
 
+def save_tasks(task):
+    with open('tasks.json', 'w', encoding='utf-8') as task_file:
+        json.dump(task, task_file, ensure_ascii=False, indent=4)
+    return
+
 
 def main():
     try:
